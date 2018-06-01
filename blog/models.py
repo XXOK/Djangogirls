2264b6ff04bcf99d272263db5b0f0cf2dbf4d275
kang <slash@zigbang.com>
2018-06-01 17:43:45 +0900

    post_detail > disqus 적용

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.utils import timezone
+from django.shortcuts import reverse
 from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    # author = models.ForeignKey('auth.User', on_delete=models.CASCADE, null=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
@@ -15,6 +16,11 @@ class Post(models.Model):
     def publish(self):
         self.published_date = timezone.now()
         self.save()
+
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[self.pk])
+
 
     def __str__(self):
         return self.title
